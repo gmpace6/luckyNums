832eb603d37ecfd75bfd35a34f461f232928a3a7
Garrett Pace <elderpace6@gmail.com>
2023-04-20 10:07:16 -0600

    n

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -6,5 +6,13 @@ result.push(Math.floor(Math.random() * 10 + 1))
     }
     return result
 }
-console.log(luckyNumbers(10))
+console.log(luckyNumbers(20))
 
+function luckyNumbers(n){
+    let result = []
+    for(let i = 1; i < n; i++){
+result.push(Math.floor(Math.random() * 10 + 1))
+    }
+    return result
+}
+console.log(luckyNumbers(20))
